6049e25cce4d75a74f2a1c5b875ce950f2b15ecb
Hidenori Miyoshi <miyohide@gmail.com>
2014-01-11 20:22:02 +0900

    add special character sample

--- spec/CoSVON/reader_spec.rb
+++ spec/CoSVON/reader_spec.rb
@@ -4,26 +4,55 @@ describe 'Cosvon::Reader' do
    describe '#load' do
       subject(:data) { reader.load(sample) }
       let(:reader) { Cosvon::Reader.new }
-      let(:sample) { <<-EOS
+
+      context 'printable ascii only' do
+         let(:sample) { <<-EOS
 CoSVON:0.1
 DreamCast,SEGA
 HI-Saturn,Hitachi
 FamiconTV,Sharp
-                     EOS
-      }
+                        EOS
+         }
 
-      it 'サンプルを Hash に変換できる' do
-         expect(subject.class).to eq(Hash)
+         it 'サンプルを Hash に変換できる' do
+            expect(subject.class).to eq(Hash)
+         end
+
+         it 'HashのKeyが正しいこと' do
+            expect(subject.keys).to match_array(%w(DreamCast HI-Saturn FamiconTV))
+         end
+
+         it 'Hashの値が正しいこと' do
+            expect(subject["DreamCast"]).to eq("SEGA")
+            expect(subject["HI-Saturn"]).to eq("Hitachi")
+            expect(subject["FamiconTV"]).to eq("Sharp")
+         end
       end
 
-      it 'HashのKeyが正しいこと' do
-         expect(subject.keys).to match_array(%w(DreamCast HI-Saturn FamiconTV))
-      end
+      context 'include special characters', focus: true do
+         let(:sample) { <<-EOS
+CoSVON:0.1
+comma,"[,]"
+dq,"[""]"
+n,"[\n]"
+tab,[\t]
+                        EOS
+         }
 
-      it 'Hashの値が正しいこと' do
-         expect(subject["DreamCast"]).to eq("SEGA")
-         expect(subject["HI-Saturn"]).to eq("Hitachi")
-         expect(subject["FamiconTV"]).to eq("Sharp")
+         it 'サンプルを Hash に変換できる' do
+            expect(subject.class).to eq(Hash)
+         end
+
+         it 'HashのKeyが正しいこと' do
+            expect(subject.keys).to match_array(%w(comma dq n tab))
+         end
+         
+         it 'Hashの値が正しいこと' do
+            expect(subject["comma"]).to eq("[,]")
+            expect(subject["dq"]).to eq('["]')
+            expect(subject["n"]).to eq("[\n]")
+            expect(subject["tab"]).to eq("[\t]")
+         end
       end
    end
 end
