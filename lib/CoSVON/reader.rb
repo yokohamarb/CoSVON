e92eaad89cd14820ff752b903ca0f6e255718ea9
Hidenori Miyoshi <miyohide@gmail.com>
2014-01-11 19:54:59 +0900

    Implement first example.

--- lib/CoSVON/reader.rb
+++ lib/CoSVON/reader.rb
@@ -1,6 +1,14 @@
+require 'csv'
 class Cosvon::Reader
    def load(data)
-      {}
+      tmp = CSV.parse(data)
+      result = {}
+      tmp.shift
+      tmp.each do |d|
+         key = d[0]
+         value = d[1]
+         result[key] = value
+      end
+      result
    end
-
 end
